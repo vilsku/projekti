d7e54f9bffe51b1896d2dafd910a41cd8a2b0956
hexarules <hexarules@gmail.com>
2019-12-12 21:17:50 +0200

    tag haku toimii OIKEESTI

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,44 +5,51 @@ const ul = document.querySelector('ul');
 
 
 const getSearch = async () => {
-  $("li").remove();
-  const response = await fetch(url + '/img/search/'+$("#search").val());
-  const imgs = await response.json();
-  for (const image of imgs) {
-    ul.innerHTML += `
+    $("li").remove();
+    const response = await fetch(url + '/img/search/' + $("#search").val());
+    const imgs = await response.json();
+    for (const image of imgs) {
+        ul.innerHTML += `
     <li>
         <h2>${image.name}</h2>
         <figure>
             <img src="../uploads/${image.filename}" class="resp">
         </figure>
-        <p>${image.tag1}</p>
-        <p>${image.tag2}</p>
-        <p>${image.tag3}</p>
+        <p class="tag">${image.tag1}</p>
+        <p class="tag">${image.tag2}</p>
+        <p class="tag">${image.tag3}</p>
     </li>
     `;
-  }
+    }
 };
 
 
 const getImg = async () => {
-  $("li").remove();
-  const response = await fetch(url + '/img');
-  const imgs = await response.json();
-  for (const img of imgs) {
-    ul.innerHTML += `
+    $("li").remove();
+    const response = await fetch(url + '/img');
+    const imgs = await response.json();
+    for (const img of imgs) {
+        ul.innerHTML += `
     <li>
         <h2>${img.name}</h2>
         <figure>
             <img src="../uploads/${img.filename}" class="resp">
         </figure>
-        <p>${img.tag1}</p>
-        <p>${img.tag2}</p>
-        <p>${img.tag3}</p>
+        <p class="tag">${img.tag1}</p>
+        <p class="tag">${img.tag2}</p>
+        <p class="tag">${img.tag3}</p>
     </li>
     `;
-  }
+        $(".tag").on("click", function () {
+            console.log("click", this.innerText );
+            $("#search").val(this.innerText);
+            getSearch();
+
+        });
+    }
 };
 getImg();
-$("#searchBtn").on("click",getSearch);
-$("#logo").on("click",getImg);
+$("#searchBtn").on("click", getSearch);
+$("#logo").on("click", getImg);
+
 
