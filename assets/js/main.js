7c07746f246f5a92353a99388692d290a69dbba6
hexarules <hexarules@gmail.com>
2019-12-13 12:13:30 +0200

    css toimii OIKEESTI

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,46 +5,58 @@ const ul = document.querySelector('ul');
 
 
 const getSearch = async () => {
-  $("li").remove();
-  const response = await fetch(url + '/img/search/' + $("#search").val());
-  const imgs = await response.json();
-  for (const image of imgs) {
-    ul.innerHTML += `
+    $("li").remove();
+    const response = await fetch(url + '/img/search/' + $("#search").val());
+    const imgs = await response.json();
+    for (const image of imgs) {
+        ul.innerHTML += `
     <li>
         <h2>${image.name}</h2>
         <figure>
-            <img src="../uploads/${image.filename}" class="flex-item">
+            <img src="../uploads/${image.filename}" class="resp">
         </figure>
-        <p class="tag">${image.tag1}</p> <p class="tag">${image.tag2}</p> <p class="tag">${image.tag3}</p>
+        <div class="tagContainer">
+        <p class="tag">${image.tag1}</p>
+        <p class="tag">${image.tag2}</p> 
+        <p class="tag">${image.tag3}</p>
+        </div>
     </li>
     `;
-  }
+        $(".tag").on("click", function () {
+            console.log("click", this.innerText);
+            $("#search").val(this.innerText);
+            getSearch();
+
+        });
+    }
 };
 
 
 const getImg = async () => {
-  $("li").remove();
-  const response = await fetch(url + '/img');
-  const imgs = await response.json();
-  for (const img of imgs) {
-    ul.innerHTML += `
+    $("li").remove();
+    const response = await fetch(url + '/img');
+    const imgs = await response.json();
+    for (const img of imgs) {
+        ul.innerHTML += `
     <li>
         <h2>${img.name}</h2>
         <figure>
             <img src="../uploads/${img.filename}" class="resp">
         </figure>
+        <div class="tagContainer">
         <p class="tag">${img.tag1}</p>
-        <p class="tag">${img.tag2}</p>
+        <p class="tag">${img.tag2}</p> 
         <p class="tag">${img.tag3}</p>
+        </div>
     </li>
     `;
-    $(".tag").on("click", function () {
-      console.log("click", this.innerText );
-      $("#search").val(this.innerText);
-      getSearch();
+        $(".tag").on("click", function () {
+            console.log("click", this.innerText);
+            $("#search").val(this.innerText);
+            getSearch();
 
-    });
-  }
+        });
+    }
 };
 getImg();
 $("#searchBtn").on("click", getSearch);
